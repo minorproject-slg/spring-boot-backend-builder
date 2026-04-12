f1cf6abfc4243ae6972cc92b44f41d433539eba2
minorproject-slg <minorproject.slg@gmail.com>
2026-04-12 17:15:37 +0530

    deleted xyflow/react

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import { Routes, Route } from "react-router";
-import Layout from "./Layout.tsx";
 import Home from "./pages/Home.tsx";
 
 function App() {
   return (
     <Routes>
-      <Route element={<Layout />}>
-        <Route path="/" element={<Home/>} />
-        <Route path="*" element={<h1>Page Not Found</h1>} />
-      </Route>
+      <Route path="/" element={<Home />} />
+      <Route path="*" element={<h1>Page Not Found</h1>} />
     </Routes>
   );
 }
